fdf656ec2b9a223ddc5434705c027e02848593f0
vyten <vytenisbucius@gmail.com>
2022-02-06 19:13:02 +0200

    Version 13 -Implementing bookData dynamic list

--- app/src/main/java/com/vb/bitfinexapi/CoinListView.kt
+++ app/src/main/java/com/vb/bitfinexapi/CoinListView.kt
@@ -1,16 +1,22 @@
 package com.vb.bitfinexapi
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import com.vb.bitfinexapi.model.BookModel
 import com.vb.bitfinexapi.model.TickerModel
 import com.vb.bitfinexapi.viewmodel.TickerViewModel
 
 
 @Composable
 fun CoinListView(tickerViewModel: TickerViewModel) {
-    val data = tickerViewModel.tickerData.collectAsState(
+    val tickerData = tickerViewModel.tickerData.collectAsState(
         TickerModel(
             bid = "0",
             bidSize = "0",
@@ -24,16 +30,35 @@ fun CoinListView(tickerViewModel: TickerViewModel) {
             low = "0"
         )
     )
+    var bookData = tickerViewModel.bookData.collectAsState(initial =
+    BookModel(
+        price = "0",
+        count = "0",
+        amount = "0"
+    ))
 
-    var tickerData = data.value
+Column() {
     Column() {
-        Text("Last Price : ${tickerData?.lastPrice}")
-        Text("Volume : ${tickerData?.volume}")
-        Text("Low : ${tickerData?.low}")
-        Text("High : ${tickerData?.high}")
-        Text("Daily Change : ${tickerData?.dailyChange}")
+        Text("Last Price : ${tickerData?.value.lastPrice}")
+        Text("Volume : ${tickerData?.value.volume}")
+        Text("Low : ${tickerData?.value.low}")
+        Text("High : ${tickerData?.value.high}")
+        Text("Daily Change : ${tickerData?.value.dailyChange}")
     }
 
+//    var displayList: ArrayList<BookModel> = ArrayList()
+
+//    val displayList = ArrayList<BookModel>()
+    val list =  remember { mutableStateListOf<BookModel>() }
+    list.add(bookData.value)
+    Log.v("list", list[0].toString())
+    if (list.size>1) {      Log.v("list2", list[1].toString()) }
 
 
-}
+    Text(bookData.value.amount)
+    LazyColumn() {
+        item {
+
+        }
+    }
+}}
